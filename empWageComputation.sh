1573b5e45575f847fc117196f7c28455eb23ee56
Vineesha-1 <vineesha.algam1218@gmail.com>
2021-09-06 18:32:32 +0530

    WageComputation added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,18 @@
 #! /bin/bash -x
-
-IS_PRESENT=1
-empCheck=$((RANDOM%2))
-if [ $empCheck -eq $IS_PRESENT ]
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
+EMP_RATE_PER_HR=20
+empCheck=$((RANDOM%3))
+if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
 then
-   echo " employee is present "
+   empHr=8
+elif [ $empCheck -eq $IS_PRESENT_PART_TIME ]
+then
+   empHr=4
 else
-   echo " employee is absent "
+   empHr=0
 fi
+ salary=$((empHr*EMP_RATE_PER_HR))
+
 
 
